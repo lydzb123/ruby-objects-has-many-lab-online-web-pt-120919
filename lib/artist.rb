fe573f791dfa159ccb33370b7a511ad68a496b77
Lydia B <lydiabaik@gmail.com>
2020-01-24 04:39:01 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,20 +1,13 @@
 class Artist
-  attr_accessor :name, :songs
+  attr_accessor :name
 
   def initialize(name)
     @name = name
-    @songs = []
   end
 
   def songs
     @songs
   end
 
-  def add_song(song)
-    self.songs = song
-  end
-
-  def add_song_by_name(song)
-  end
 
 end
